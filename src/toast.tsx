8c93da4a863a3914d0bcec3f14636d2661c355ce
Luuk de Weijer <luuk@scrumble.nl>
2020-03-28 14:48:56 +0100

    final changes

--- src/toast.tsx
+++ src/toast.tsx
@@ -12,7 +12,6 @@ interface props {
 }
 
 export default class Toast extends React.Component<props, {}> {
-    
     componentDidMount = (): void => {
         let {dismissTimer, id} = this.props.toast;
 
